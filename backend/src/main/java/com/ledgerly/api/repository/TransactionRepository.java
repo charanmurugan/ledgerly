cbc1d6b79e558b44d2acb3a12a91b744662af2ae
Charan M <71169276+charanmurugan@users.noreply.github.com>
2026-09-05 23:15:42 +0530

    Delete backend directory

--- backend/src/main/java/com/ledgerly/api/repository/TransactionRepository.java
+++
@@ -1,5 +0,0 @@
-package com.ledgerly.api.repository;
-import com.ledgerly.api.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-public interface TransactionRepository extends JpaRepository<Transaction,UUID>{}
